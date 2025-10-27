bcb5cdd2d7645d1a7cf991e2ceb9b79ee2ea2957
J. Sebastian Duque <info@jsebastiandq.com>
2025-10-27 11:10:15 -0500

    Agregamos env y Sequealize para usar ORM MYSQL

--- index.js
+++ index.js
@@ -1,8 +1,17 @@
 import express from "express";
 import usuarioRoutes from "./routes/usuariosRoutes.js";
+import db from "./config/db.js";
 
 // Crear APP
 const app = express();
+
+// Conexion a la DB
+try {
+  await db.authenticate();
+  console.log("La conexion es correcta a la DB");
+} catch (error) {
+  console.error("No se puede conectar", error);
+}
 
 // Habilitar Pug
 app.set("view engine", "pug");
@@ -15,7 +24,7 @@ app.use(express.static("public"));
 app.use("/auth", usuarioRoutes);
 
 // Definir el puerto
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 app.listen(port, () => {
   console.log("El servidor esta corriendo en el puerto: " + port);
