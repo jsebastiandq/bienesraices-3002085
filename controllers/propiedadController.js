25f63f44508a14452324f3f133c0d9c043065c85
J. Sebastian Duque <info@jsebastiandq.com>
2025-11-11 11:29:09 -0500

    Plantilla de Crear Propiedad

--- controllers/propiedadController.js
+++ controllers/propiedadController.js
@@ -6,4 +6,12 @@ const admin = (req, res) => {
   });
 };
 
-export { admin };
+const crear = (req, res) => {
+  res.render("propiedades/crear", {
+    tituloPagina: "Crear una nueva propiedad",
+    csrfToken: req.csrfToken(),
+    headerAdmin: true,
+  });
+};
+
+export { admin, crear };
--- routes/propiedadesRoutes.js
+++ routes/propiedadesRoutes.js
@@ -1,9 +1,10 @@
 import express from "express";
-import { admin } from "../controllers/propiedadController.js";
+import { admin, crear } from "../controllers/propiedadController.js";
 
 const router = express.Router();
 
 // Mis propidades
 router.get("/mis-propiedades", admin);
+router.get("/propiedades/crear", crear);
 
 export default router;
