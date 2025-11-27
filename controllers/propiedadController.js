030ab3a9efce2528b784581c5c1b2d30f88710b8
J. Sebastian Duque <info@jsebastiandq.com>
2025-11-27 08:11:55 -0500

    Seccion editar y eliminar completas

--- controllers/propiedadController.js
+++ controllers/propiedadController.js
@@ -1,3 +1,4 @@
+import { unlink } from "node:fs/promises";
 import { Precios, Categorias, Usuarios, Propiedades } from "../models/index.js";
 import { validationResult } from "express-validator";
 
@@ -186,4 +187,137 @@ const almacenarImagen = async (req, res, next) => {
   }
 };
 
-export { admin, crear, guardar, agregarImagen, almacenarImagen };
+const editar = async (req, res) => {
+  const { id } = req.params;
+
+  // Validar que la propiedad exista
+  const propiedad = await Propiedades.findByPk(id);
+
+  if (!propiedad) {
+    return res.redirect("/mis-propiedades");
+  }
+
+  // Revisar que quien visita la URl, es quien creo la propiedad
+  if (propiedad.usuarioId.toString() !== req.usuario.id.toString()) {
+    return res.redirect("/mis-propiedades");
+  }
+
+  // Consultar Modelo de Precio y Categorias
+  const [categorias, precios] = await Promise.all([
+    Categorias.findAll(),
+    Precios.findAll(),
+  ]);
+
+  res.render("propiedades/editar", {
+    pagina: `Editar Propiedad: ${propiedad.titulo}`,
+    csrfToken: req.csrfToken(),
+    categorias,
+    precios,
+    datos: propiedad,
+  });
+};
+
+const guardarCambios = async (req, res) => {
+  // Verificar la validación
+  let resultado = validationResult(req);
+
+  if (!resultado.isEmpty()) {
+    // Consultar Modelo de Precio y Categorias
+    const [categorias, precios] = await Promise.all([
+      Categorias.findAll(),
+      Precios.findAll(),
+    ]);
+
+    return res.render("propiedades/editar", {
+      pagina: "Editar Propiedad",
+      csrfToken: req.csrfToken(),
+      categorias,
+      precios,
+      errores: resultado.array(),
+      datos: req.body,
+    });
+  }
+
+  const { id } = req.params;
+
+  // Validar que la propiedad exista
+  const propiedad = await Propiedades.findByPk(id);
+
+  if (!propiedad) {
+    return res.redirect("/mis-propiedades");
+  }
+
+  // Revisar que quien visita la URl, es quien creo la propiedad
+  if (propiedad.usuarioId.toString() !== req.usuario.id.toString()) {
+    return res.redirect("/mis-propiedades");
+  }
+
+  // Reescribir el objeto y actualizarlo
+  try {
+    const {
+      titulo,
+      descripcion,
+      habitaciones,
+      estacionamiento,
+      wc,
+      calle,
+      lat,
+      lng,
+      precio: precioId,
+      categoria: categoriaId,
+    } = req.body;
+
+    propiedad.set({
+      titulo,
+      descripcion,
+      habitaciones,
+      estacionamiento,
+      wc,
+      calle,
+      lat,
+      lng,
+      precioId,
+      categoriaId,
+    });
+
+    await propiedad.save();
+
+    res.redirect("/mis-propiedades");
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+const eliminar = async (req, res) => {
+  const { id } = req.params;
+
+  // Validar que la propiedad exista
+  const propiedad = await Propiedades.findByPk(id);
+  if (!propiedad) {
+    return res.redirect("/mis-propiedades");
+  }
+
+  // Revisar que quien visita la URl, es quien creo la propiedad
+  if (propiedad.usuarioId.toString() !== req.usuario.id.toString()) {
+    return res.redirect("/mis-propiedades");
+  }
+
+  // Eliminar la imagen
+  await unlink(`public/uploads/${propiedad.imagen}`);
+  console.log(`Se eliminó la imagen ${propiedad.imagen}`);
+
+  // Eliminar la propiedad
+  await propiedad.destroy();
+  res.redirect("/mis-propiedades");
+};
+
+export {
+  admin,
+  crear,
+  guardar,
+  agregarImagen,
+  almacenarImagen,
+  editar,
+  guardarCambios,
+  eliminar,
+};
--- routes/propiedadesRoutes.js
+++ routes/propiedadesRoutes.js
@@ -6,6 +6,9 @@ import {
   guardar,
   agregarImagen,
   almacenarImagen,
+  editar,
+  guardarCambios,
+  eliminar,
 } from "../controllers/propiedadController.js";
 import protegerRuta from "../middleware/protegerRuta.js";
 import upload from "../middleware/subirImagen.js";
@@ -44,5 +47,31 @@ router.post(
   upload.single("imagen"),
   almacenarImagen
 );
+
+router.get("/propiedades/editar/:id", protegerRuta, editar);
+
+router.post(
+  "/propiedades/editar/:id",
+  protegerRuta,
+  body("titulo").notEmpty().withMessage("El Titulo del Anuncio es Obligatorio"),
+  body("descripcion")
+    .notEmpty()
+    .withMessage("La Descripción no puede ir vacia")
+    .isLength({ max: 200 })
+    .withMessage("La Descripción es muy larga"),
+  body("categoria").isNumeric().withMessage("Selecciona una categoría"),
+  body("precio").isNumeric().withMessage("Selecciona un rango de Precios"),
+  body("habitaciones")
+    .isNumeric()
+    .withMessage("Selecciona la Cantidad de Habitaciones"),
+  body("estacionamiento")
+    .isNumeric()
+    .withMessage("Selecciona la Cantidad de Estacionamientos"),
+  body("wc").isNumeric().withMessage("Selecciona la Cantidad de Baños"),
+  body("lat").notEmpty().withMessage("Ubica la Propiedad en el Mapa"),
+  guardarCambios
+);
+
+router.post("/propiedades/eliminar/:id", protegerRuta, eliminar);
 
 export default router;
